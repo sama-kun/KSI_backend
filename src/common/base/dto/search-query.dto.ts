ac8e334481be6dad53f4496107bde9fe6c16afc5
sama-kun <samgar.robot@gmail.com>
2023-08-21 09:51:14 +0600

    fixed prisma deleted

--- src/common/base/dto/search-query.dto.ts
+++ src/common/base/dto/search-query.dto.ts
@@ -1,7 +1,5 @@
-import { IntersectionType, PartialType } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsInt } from 'class-validator';
-import { Prisma, User } from '@prisma/client';
 
 export class SearchQueryDto {
   pagination?: Pagination;
