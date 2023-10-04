ee6ed62fe041406d22dab568ff049621785d9c26
sama-kun <samgar.robot@gmail.com>
2023-10-04 16:31:58 +0600

    build

--- dist/src/modules/project/dto/search-project.dto.d.ts
+++ dist/src/modules/project/dto/search-project.dto.d.ts
@@ -1,6 +1,6 @@
 import { SearchQueryDto } from '@/common/base/dto/search-query.dto';
 import { ProjectEntity } from '@/database/entities/project.entity';
-declare const SearchProjectDto_base: import("@nestjs/common").Type<Partial<ProjectEntity & SearchQueryDto>>;
+declare const SearchProjectDto_base: import("@nestjs/common").Type<Partial<SearchQueryDto & ProjectEntity>>;
 export declare class SearchProjectDto extends SearchProjectDto_base {
     sort?: Partial<ProjectEntity>;
     filter?: Partial<ProjectEntity>;
