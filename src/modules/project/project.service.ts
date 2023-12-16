ae1fa5ec01a849d2e9d1b5bf9d45f6488fc574cf
sama-kun <samgar.robot@gmail.com>
2023-12-16 21:18:36 +0600

    test poppurer

--- src/modules/project/project.service.ts
+++ src/modules/project/project.service.ts
@@ -54,7 +54,7 @@ export class ProjectService extends BaseService<
       headless: true,
       ignoreDefaultArgs: ['--disable-extensions'],
       // executablePath: await chromium.executablePath,
-      userDataDir: '../../../workspace/.cache/puppeteer',
+      userDataDir: '../../../.cache/puppeteer',
       // channel: await chromium.executablePath,
       //../../..
     });
