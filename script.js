04c80988a281b484b4b4bb3eea63fa17c527d3d1
sama-kun <samgar.robot@gmail.com>
2023-12-19 18:14:50 +0600

    test poppurer

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ console.log('Running custom build script...');
 
 // Install libnss3 or perform any other necessary setup
 try {
-  execSync('apt-get update');
-  execSync('apt-get install -y libnss3');
+  execSync('sudo apt-get install -y libnss3');
 } catch (error) {
   console.error('Failed to install libnss3:', error.message);
   process.exit(1);
